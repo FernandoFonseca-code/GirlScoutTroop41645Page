6c7592ec0b51d254d0381f113663e0349b7a6814
FernandoFonseca-code <fernando.fonseca2021@gmail.com>
2025-04-06 16:56:41 -0700

    Add project files.

---
+++ GirlScoutTroop41645Page/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GirlScoutTroop41645Page.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
