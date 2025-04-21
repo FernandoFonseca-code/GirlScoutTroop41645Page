fc41ed76a868f82d3b0e9b7c29022c1f84ce19de
FernandoFonseca-code <fernando.fonseca2021@gmail.com>
2025-04-21 12:41:46 -0700

    Add authorization to HomeController
    Implement [Authorize] attribute for HomeController to secure access.
    Add summary comment regarding website security.
    Include necessary using directive for authorization while keeping
    the Index action method accessible to anonymous users.

--- GirlScoutTroop41645Page/Controllers/HomeController.cs
+++ GirlScoutTroop41645Page/Controllers/HomeController.cs
@@ -1,9 +1,13 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using GirlScoutTroop41645Page.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace GirlScoutTroop41645Page.Controllers;
-
+/// <summary>
+/// Rest of website is secured behind login credentials
+/// </summary>
+[Authorize]
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
@@ -12,7 +16,8 @@ public class HomeController : Controller
     {
         _logger = logger;
     }
-
+    // Can view home page without a login (PUBLIC)
+    [AllowAnonymous]
     public IActionResult Index()
     {
         return View();
